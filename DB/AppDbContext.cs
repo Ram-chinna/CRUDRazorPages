d5c3f0cc0aea2d8d78605b854983b4b63a27bac5
Administrator <Administrator@DESKTOP-CVDCG4C>
2024-06-30 11:01:59 +0530

    Add project files.

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUDRazorPages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDRazorPages.DB
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+        }
+		public virtual DbSet<Product> Products { get; set; }
+	}
+}
---
+++ Pages/ProductMaster/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUDRazorPages.DB;
+using CRUDRazorPages.Models;
+
+namespace CRUDRazorPages.Pages.ProductMaster
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CRUDRazorPages.DB.AppDbContext _context;
+
+        public IndexModel(CRUDRazorPages.DB.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
